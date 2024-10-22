2129fe1cd6b6a5ff268b35fc9d601f14eb44763e
Christian Real <146919289+realchristian2003@users.noreply.github.com>
2024-10-22 00:35:45 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="header-text">Notes</div>
+        <button className="add-note-button">➕Add note</button>
       </header>
+      <main className="App-main">
+        {/* Empty content area, colored gray */}
+      </main>
     </div>
   );
 }
